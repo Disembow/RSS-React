34e416eb319e8f9d8db6e3d479ebd6894b9a0b34
Disembow <e_naletov@mail.ru>
2023-03-17 14:29:22 +0100

    init: configure the app

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './main.css';
+
+const root: HTMLDivElement | null = document.querySelector('#root');
+if (root)
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
