84d851ff0f9df95e251fccb086174ecd76f9e92a
Disembow <e_naletov@mail.ru>
2023-03-19 07:36:17 +0100

    feat: change router logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,31 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom';
 import './App.css';
 
 // Components
 import Title from './components/Title';
 import Footer from './components/Footer';
+import RootLayout from './components/layouts/RootLayout';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Title>{'Home page'}</Title>}></Route>
+      <Route path="about" element={<Title>{'About'}</Title>}></Route>
+      <Route path="*" element={<Title>{'Page not found'}</Title>}></Route>
+    </Route>
+  )
+);
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <header className="header">
-          <Routes>
-            <Route index element={<Title>{'Home page'}</Title>}></Route>
-            <Route path="about" element={<Title>{'About'}</Title>}></Route>
-            <Route path="*" element={<Title>{'Page not found'}</Title>}></Route>
-          </Routes>
-          <nav className="navigation">
-            <NavLink to="/" className={'navigation__item'}>
-              Home
-            </NavLink>
-            <NavLink to="about" className={'navigation__item'}>
-              About Us
-            </NavLink>
-          </nav>
-        </header>
-        <main className="main"></main>
-      </BrowserRouter>
+      <RouterProvider router={router} />
       <Footer />
     </>
   );
---
+++ src/components/layouts/RootLayout.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
+
+export default function RootLayout() {
+  return (
+    <div className="root__layout">
+      <header>
+        <nav className="navigation">
+          <NavLink to="/" className={'navigation__item'}>
+            Home
+          </NavLink>
+          <NavLink to="about" className={'navigation__item'}>
+            About Us
+          </NavLink>
+        </nav>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
