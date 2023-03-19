7aeb3876a4ea29d9738c9ab49ea5bca5edb9cd5d
Disembow <e_naletov@mail.ru>
2023-03-19 13:27:38 +0100

    feat: add searchbar & implement LS saving

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,14 @@ import './App.css';
 
 // Components
 import Title from './components/Title';
+import Main from './components/Main';
 import Footer from './components/Footer';
 import RootLayout from './components/layouts/RootLayout';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
-      <Route index element={<Title>{'Home page'}</Title>}></Route>
+      <Route index element={<Main />}></Route>
       <Route path="about" element={<Title>{'About'}</Title>}></Route>
       <Route path="*" element={<Title>{'Page not found'}</Title>}></Route>
     </Route>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,43 @@
 import React from 'react';
+import Title from './Title';
 import './Main.css';
 
-export default function Main() {
-  return <main className="main">Main</main>;
+type TState = {
+  input: string;
+};
+
+export default class Main extends React.Component {
+  state: TState = { input: localStorage.getItem('RSTaskMessage') || '' };
+
+  componentWillUnmount(): void {
+    localStorage.setItem('RSTaskMessage', JSON.stringify(JSON.parse(this.state.input)));
+  }
+
+  handleInputValue(e: React.ChangeEvent<HTMLInputElement>): void {
+    e.preventDefault();
+    this.setState({ input: e.target.value });
+    console.log(e.target.value);
+  }
+
+  getLSData(): string {
+    return this.state.input ? JSON.parse(this.state.input) : '';
+  }
+
+  render() {
+    return (
+      <main className="main">
+        <Title>{'Home page'}</Title>
+        <div className="search light-block">
+          <input
+            className="search__input"
+            type="text"
+            placeholder="Search"
+            value={this.getLSData()}
+            onChange={this.handleInputValue}
+          />
+          <span className="search__icon"></span>
+        </div>
+      </main>
+    );
+  }
 }
