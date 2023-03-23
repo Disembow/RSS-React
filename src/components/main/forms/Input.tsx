669832e1b6220498050744b7a9bfabc79a360758
Disembow <e_naletov@mail.ru>
2023-03-23 14:18:49 +0100

    feat: add radio buttons

--- src/components/main/forms/Input.tsx
+++ src/components/main/forms/Input.tsx
@@ -2,6 +2,9 @@ import React, { createRef, FormEvent, RefObject } from 'react';
 import { TProps } from '../../../types/props-types';
 import Option from './Option';
 
+type TState = {
+  radio: string;
+};
 export default class Input extends React.Component {
   inputName: RefObject<HTMLInputElement>;
   inputSecName: RefObject<HTMLInputElement>;
@@ -10,10 +13,14 @@ export default class Input extends React.Component {
   checkbox: RefObject<HTMLInputElement>;
   defaultDeliveryDate: string;
 
+  state: TState;
+
   constructor(props: TProps) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
+    this.handleRadio = this.handleRadio.bind(this);
+
     this.inputName = createRef();
     this.inputSecName = createRef();
     this.inputDate = createRef();
@@ -22,7 +29,7 @@ export default class Input extends React.Component {
     this.defaultDeliveryDate = new Date().toISOString().slice(0, 10);
 
     this.state = {
-      checkboxChecked: false,
+      radio: '',
     };
   }
 
@@ -36,6 +43,10 @@ export default class Input extends React.Component {
     status ? (status = false) : (status = true);
   };
 
+  handleRadio(e: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ radio: e.target.value });
+  }
+
   handleSubmit(e: FormEvent) {
     e.preventDefault();
     console.group('On form submit');
@@ -44,6 +55,7 @@ export default class Input extends React.Component {
     console.log('Date of delivery: ' + this.inputDate.current?.value);
     console.log('Post service: ' + this.selector.current?.value.toUpperCase());
     console.log('Checkbox: ' + this.checkbox.current?.checked);
+    console.log('Sex: ' + this.state.radio);
     console.groupEnd();
   }
 
@@ -61,6 +73,7 @@ export default class Input extends React.Component {
             name="first-name"
             autoComplete="off"
             placeholder="Enter your first name"
+            required
           />
         </div>
 
@@ -75,6 +88,7 @@ export default class Input extends React.Component {
             name="last-name"
             autoComplete="off"
             placeholder="Enter your last name"
+            required
           />
         </div>
 
@@ -91,6 +105,7 @@ export default class Input extends React.Component {
             max={`${new Date().getFullYear() + 1}${new Date().toISOString().slice(4, 10)}`}
             defaultValue={this.defaultDeliveryDate}
             onChange={this.handleDataChange}
+            required
           />
         </div>
 
@@ -107,7 +122,7 @@ export default class Input extends React.Component {
 
         <div className="form__item">
           <label className="form__label" htmlFor="data-agree-box">
-            Personal data:
+            Email notifications:
           </label>
           <input
             className="form__checkbox"
@@ -116,6 +131,12 @@ export default class Input extends React.Component {
             name="data-agree-box"
             ref={this.checkbox}
           />
+        </div>
+
+        <div className="form__item form__item_radio" onChange={this.handleRadio}>
+          <input className="form__checkbox" type="radio" value="Male" name="gender" /> Male
+          <input className="form__checkbox" type="radio" value="Female" name="gender" /> Female
+          <input className="form__checkbox" type="radio" value="Other" name="gender" /> Other
         </div>
 
         <button className="button button__submit" type="submit">
