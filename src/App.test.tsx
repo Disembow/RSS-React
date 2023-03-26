d2f77bf192e97e335feaba2041e26ebaf2fbeb25
Disembow <e_naletov@mail.ru>
2023-03-26 20:45:23 +0200

    feat: add router tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { MemoryRouter } from 'react-router';
+import { Routes, Route } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 
-// import { MemoryRouter } from 'react-router';
-
 import App from './App';
+import Home from './components/main/home/Home';
+import Title from './components/header/Title';
+import Forms from './components/main/forms/Forms';
 
 describe('App', () => {
   it('Renders Home page', () => {
@@ -16,16 +19,57 @@ describe('App', () => {
   });
 });
 
-// test('Renders Page not found if user enter wrong path', () => {
-//   const badRoute = '/wrong/way/link';
-//   render(
-//     <MemoryRouter initialEntries={[badRoute]}>
-//       <App />
-//     </MemoryRouter>
-//   );
-//   expect(
-//     screen.getByRole('heading', {
-//       level: 1,
-//     })
-//   ).toHaveTextContent('Page not found :(');
-// });
+test('Renders Page not found if user enter wrong path', () => {
+  const badRoute = '/wrong/way/link';
+  render(
+    <MemoryRouter initialEntries={[badRoute]}>
+      <Routes>
+        <Route index element={<Home />}></Route>
+        <Route path="about" element={<Title>{'About'}</Title>}></Route>
+        <Route path="forms" element={<Forms />}></Route>
+        <Route path="*" element={<Title>{'Page not found :('}</Title>}></Route>
+      </Routes>
+    </MemoryRouter>
+  );
+  expect(
+    screen.getByRole('heading', {
+      level: 1,
+    })
+  ).toHaveTextContent('Page not found :(');
+});
+
+test('Renders Page not found if user enter wrong path', () => {
+  render(
+    <MemoryRouter initialEntries={['/about']}>
+      <Routes>
+        <Route index element={<Home />}></Route>
+        <Route path="about" element={<Title>{'About'}</Title>}></Route>
+        <Route path="forms" element={<Forms />}></Route>
+        <Route path="*" element={<Title>{'Page not found :('}</Title>}></Route>
+      </Routes>
+    </MemoryRouter>
+  );
+  expect(
+    screen.getByRole('heading', {
+      level: 1,
+    })
+  ).toHaveTextContent('About');
+});
+
+test('Renders Page not found if user enter wrong path', () => {
+  render(
+    <MemoryRouter initialEntries={['/forms']}>
+      <Routes>
+        <Route index element={<Home />}></Route>
+        <Route path="about" element={<Title>{'About'}</Title>}></Route>
+        <Route path="forms" element={<Forms />}></Route>
+        <Route path="*" element={<Title>{'Page not found :('}</Title>}></Route>
+      </Routes>
+    </MemoryRouter>
+  );
+  expect(
+    screen.getByRole('heading', {
+      level: 1,
+    })
+  ).toHaveTextContent('Forms page');
+});
