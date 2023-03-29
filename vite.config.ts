a3ce56b8feac29668d8173f222d1cc9660ff6348
Disembow <e_naletov@mail.ru>
2023-03-29 12:45:24 +0200

    feat: update test config with flags

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 /// <reference types="vite/client" />
 
 import { defineConfig } from 'vite';
+import { configDefaults } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 
@@ -11,5 +12,12 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/setupTests.ts'],
+    coverage: {
+      exclude: [...(configDefaults.coverage.exclude || []), 'src/main.tsx', 'src/HOC/*.tsx'],
+      all: true,
+      src: ['src'],
+      provider: 'c8',
+      reporter: ['text'],
+    },
   },
 });
