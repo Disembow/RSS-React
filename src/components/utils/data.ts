c948dc2354c57572e378a6ba37f6ba56ff585b77
Disembow <e_naletov@mail.ru>
2023-04-16 12:16:14 +0200

    refactor: add link to deployed api

--- src/components/utils/data.ts
+++ src/components/utils/data.ts
@@ -1,7 +1,7 @@
 export const DEPLOY_API_LINK = 'https://mock-server-api-production.up.railway.app/';
 export const LOCAL_API_LINK = 'http://localhost:3000/';
 export const API_CATALOG = 'catalog';
-export const API_LINK = LOCAL_API_LINK;
+export const API_LINK = DEPLOY_API_LINK;
 
 export const defaultDeliveryDate = new Date().toISOString().slice(0, 10);
 export const maxDate = new Date(
